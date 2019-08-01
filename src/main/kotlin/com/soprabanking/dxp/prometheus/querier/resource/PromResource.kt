f3b944598fbfc3a042693565f5c4bdee3cdfcf3a
NYCZ Antonin <antonin.nycz@soprabanking.com>
2019-08-01 17:53:21 +0200

    changes ordering to have server-proxy last, adds namespace to path params, removes ui-backoffice from list

--- src/main/kotlin/com/soprabanking/dxp/prometheus/querier/resource/PromResource.kt
+++ src/main/kotlin/com/soprabanking/dxp/prometheus/querier/resource/PromResource.kt
@@ -1,10 +1,10 @@
 package com.soprabanking.dxp.prometheus.querier.resource
 
-import com.soprabanking.dxp.commons.page.Range
 import com.soprabanking.dxp.prometheus.model.api.MeasureDTO
 import com.soprabanking.dxp.prometheus.querier.client.PrometheusClient
 import com.soprabanking.dxp.prometheus.querier.service.MetricService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
@@ -25,13 +25,14 @@ class PromResource(private val client: PrometheusClient, private val service: Me
         const val URI = "/api/metrics"
     }
 
-    @GetMapping
-    fun findAll(range: Range) : Mono<String> {
+    @GetMapping("/{namespace}")
+    fun findAll(@PathVariable namespace: String): Mono<String> {
         val start = OffsetDateTime.now().minus(1, ChronoUnit.MINUTES)
-        // TODO input NS or list NS ??
-        return service.findServices(start)
-                .flatMap { it.toMeasure(start) }
-                .sort(compareBy {it.msName })
+        return service.findServices(start, namespace)
+                .filter { !listOf("busybox", "sleep", "ui-backoffice").contains(it) }
+                .flatMap { it.toMeasure(start, namespace) }
+                .sort(Comparator<MeasureDTO> { a, b -> if (a.msName == "server-proxy") 1 else if (b.msName == "server-proxy") -1 else 0 }
+                              .then(compareBy { it.msName }))
                 .map {
                     "|${it.msName}|${it.cpu.round(3)}|2|${it.ops.round(1)}|${it.p50ms.roundToLong()}|${it.p90ms.roundToLong()}|${it.p99ms.roundToLong()}|${it.successPercent.round(
                             2
@@ -41,28 +42,27 @@ class PromResource(private val client: PrometheusClient, private val service: Me
                 .doOnSuccess { println(it) }
     }
 
-    // TODO value en NaN...
-    private fun String.toMeasure(start: OffsetDateTime) =
+    private fun String.toMeasure(start: OffsetDateTime, namespace: String) =
             listOf(
-                    this.findPercentile(50f, start),
-                    this.findPercentile(90f, start),
-                    this.findPercentile(99f, start),
-                    this.findOps(start),
-                    this.findCPU(start),
-                    this.findSuccess(start)
+                    this.findPercentile(50f, start, namespace),
+                    this.findPercentile(90f, start, namespace),
+                    this.findPercentile(99f, start, namespace),
+                    this.findOps(start, namespace),
+                    this.findCPU(start, namespace),
+                    this.findSuccess(start, namespace)
             ).zip { (p50, p90, p99, ops, cpu, success) -> MeasureDTO(this, cpu, ops, p50, p90, p99, success) }
 
-    private fun String.findPercentile(percent: Float, start: OffsetDateTime) =
-            service.findPercentile(this, percent, start, start.plus(20, ChronoUnit.MINUTES), 5)
+    private fun String.findPercentile(percent: Float, start: OffsetDateTime, namespace: String) =
+            service.findPercentile(this, percent, start, start.plus(20, ChronoUnit.MINUTES), 5, namespace)
 
-    private fun String.findOps(start: OffsetDateTime) =
-            service.findOps(this, start, start.plus(20, ChronoUnit.MINUTES), 5)
+    private fun String.findOps(start: OffsetDateTime, namespace: String) =
+            service.findOps(this, start, start.plus(20, ChronoUnit.MINUTES), 5, namespace)
 
-    private fun String.findCPU(start: OffsetDateTime) =
-            service.findCPU(this, start, start.plus(20, ChronoUnit.MINUTES), 5)
+    private fun String.findCPU(start: OffsetDateTime, namespace: String) =
+            service.findCPU(this, start, start.plus(20, ChronoUnit.MINUTES), 5, namespace)
 
-    private fun String.findSuccess(start: OffsetDateTime) =
-            service.findSuccess(this, start, start.plus(20, ChronoUnit.MINUTES), 5)
+    private fun String.findSuccess(start: OffsetDateTime, namespace: String) =
+            service.findSuccess(this, start, start.plus(20, ChronoUnit.MINUTES), 5, namespace)
 
     operator fun <T> List<T>.component6() = this[5]
 
